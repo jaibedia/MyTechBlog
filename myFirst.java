9d9d81ae5fe2d9a60b2fcf99dc52107ddbc7311c
Jai Bedia <jaibedia8@gmail.com>
2023-10-15 22:27:52 +0530

    add new class

---
+++ myFirst.java
@@ -0,0 +1,28 @@
+package MyTechBlog;
+
+class Sleep extends Thread{
+
+    @Override
+    public void run() {
+        for(int i=0; i<5;i++){
+            System.out.println("Hello jai...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+ class myFirst {
+    private static Sleep sleep;
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+       Sleep sleep = new Sleep();
+       sleep.setPriority(10);
+       sleep.start();
+    }
+
+}
